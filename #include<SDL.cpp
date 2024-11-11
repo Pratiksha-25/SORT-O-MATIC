3942ed0094844fd3e7e0c81ab69c1722aa6c3238
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2024-11-11 16:44:39 +0530

    Update #include<SDL.cpp

--- #include<SDL.cpp
+++ #include<SDL.cpp
@@ -159,6 +159,127 @@ void inplaceHeapSort(int* input, int n)
         }
     }
 }
+int partition_array(int a[], int si, int ei)
+{
+    int count_small=0;
+
+    for(int i=(si+1);i<=ei;i++)
+    {
+        if(a[i]<=a[si])
+        {
+            count_small++;
+        }
+    }
+    int c=si+count_small;
+    int temp=a[c];
+    a[c]=a[si];
+    a[si]=temp;
+    visualize(c, si);
+
+    int i=si, j=ei;
+
+    while(i<c && j>c)
+    {
+        if(a[i]<= a[c])
+        {
+            i++;
+        }
+        else if(a[j]>a[c])
+        {
+            j--;
+        }
+        else
+        {
+            int temp_1=a[j];
+            a[j]=a[i];
+            a[i]=temp_1;
+
+            visualize(i, j);
+            SDL_Delay(70);
+
+            i++;
+            j--;
+        }
+    }
+    return c;
+}
+
+void quickSort(int a[], int si, int ei)
+{
+    if(si>=ei)
+    {
+        return;
+    }
+
+    int c=partition_array(a, si, ei);
+    quickSort(a, si, c-1);
+    quickSort(a, c+1, ei);
+
+}
+void merge2SortedArrays(int a[], int si, int ei)
+{
+    int size_output=(ei-si)+1;
+    int* output=new int[size_output];
+
+    int mid=(si+ei)/2;
+    int i=si, j=mid+1, k=0;
+    while(i<=mid && j<=ei)
+    {
+        if(a[i]<=a[j])
+        {
+            output[k]=a[i];
+            visualize(i, j);
+            i++;
+            k++;
+        }
+        else
+        {
+            output[k]=a[j];
+            visualize(i, j);
+            j++;
+            k++;
+        }
+
+    }
+    while(i<=mid)
+    {
+        output[k]=a[i];
+        visualize(-1, i);
+        i++;
+        k++;
+    }
+    while(j<=ei)
+    {
+        output[k]=a[j];
+        visualize(-1, j);
+        j++;
+        k++;
+    }
+    int x=0;
+    for(int l=si; l<=ei; l++)
+    {
+        a[l]=output[x];
+        visualize(l);
+        SDL_Delay(15);
+        x++;
+    }
+    delete []output;
+}
+void mergeSort(int a[], int si, int ei)
+{
+    if(si>=ei)
+    {
+        return;
+    }
+    int mid=(si+ei)/2;
+
+    mergeSort(a, si, mid);
+    mergeSort(a, mid+1, ei);
+
+    merge2SortedArrays(a, si, ei);
+}
+
+
 
 
     
